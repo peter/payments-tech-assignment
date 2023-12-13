5da8616e9344a68cb3e88b5eda72397acc1dd5c4
Peter Marklund <peter@marklunds.com>
2023-12-13 12:03:22 +0100

    add more tests for PriceHistory.call

--- spec/lib/price_history_spec.rb
+++ spec/lib/price_history_spec.rb
@@ -6,6 +6,37 @@ RSpec.describe PriceHistory do
   # These tests cover feature request 2. Feel free to add more tests or change
   # the existing ones.
 
+  it "throws an error if a package is not provided" do
+    expect {
+      PriceHistory.call()
+    }.to raise_error(ArgumentError)
+  end
+
+  it "returns the pricing history for only a package" do
+    basic = Package.create!(name: "basic")
+
+    UpdatePackagePrice.call(basic, 20_00)
+
+    travel_to Time.zone.local(2019) do
+      # These should NOT be included
+      UpdatePackagePrice.call(basic, 20_00, municipality: "Stockholm")
+      UpdatePackagePrice.call(basic, 30_00, municipality: "Göteborg")
+    end
+
+    travel_to Time.zone.local(2020) do
+      UpdatePackagePrice.call(basic, 30_00, municipality: "Stockholm")
+      UpdatePackagePrice.call(basic, 40_00, municipality: "Stockholm")
+      UpdatePackagePrice.call(basic, 100_00, municipality: "Göteborg")
+    end
+
+    history = PriceHistory.call(package: basic)
+    expect(history).to eq(
+      nil => [0, 20_00],
+      "Göteborg" => [30_00, 100_00],
+      "Stockholm" => [20_00, 30_00, 40_00],
+    )
+  end
+
   it "returns the pricing history for the provided year and package" do
     basic = Package.create!(name: "basic")
 
